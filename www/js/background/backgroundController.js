f355869b60027e71b1670877ae8b31b52c42f8f2
Leonardo Picoli <lgpicoli@hotmail.com>
2020-09-29 14:04:33 -0300

    final p5

--- www/js/background/backgroundController.js
+++ www/js/background/backgroundController.js
@@ -2,7 +2,7 @@ class BackgroundController {
     constructor(num_particles){
         this.particles = [];
 
-        createCanvas(windowWidth, windowHeight);
+        createCanvas(windowWidth, windowHeight, WEBGL);
 
         /* Control background particles */
         for(let i = 0;i<num_particles;i++){
@@ -13,10 +13,10 @@ class BackgroundController {
     draw() {
         background('rgba(0,100,255,0.7)');
 
-        for(let i = 0;i<this.particles.length;i++) {
+        /*for(let i = 0;i<this.particles.length;i++) {
             this.particles[i].createParticle();
             this.particles[i].moveParticle();
             this.particles[i].joinParticles(this.particles.slice(i));
-        }
+        }*/
     }
 }
